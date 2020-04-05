044a225d142d977d4f8ea3ca3104a060155f471f
Artyom <neshi2004@yandex.ru>
2020-04-05 15:25:31 +0300

    Это наш первый коммит

---
+++ javascript-example.js
@@ -0,0 +1,18 @@
+"use strict"
+// var obj = {
+//     name: 'Artem'
+// };
+// console.log(obj['name']);
+
+// console.log(typeof("d" + 54));
+
+// console.log(5+ +true);
+
+// console.log(parseInt('15px', 10));
+// let answer = prompt('Есть ли вам 18 лет', '');
+let incr = 10,
+    decr = 10;
+incr++;
+--decr;
+console.log(incr);
+console.log(decr);
