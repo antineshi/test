6679ff8fa4cec919e865431c5de0de0f9e6b30b4
Artyom <neshi2004@yandex.ru>
2020-04-09 15:11:16 +0300

    Урок 2 на циклы

--- javascript-example.js
+++ javascript-example.js
@@ -1,18 +1,35 @@
 "use strict"
-// var obj = {
-//     name: 'Artem'
-// };
-// console.log(obj['name']);
+let money = +prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD"),
+    i = 0;
+    
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+do {
+    let topic = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        cash = +prompt('Во сколько обойдется?', '');
+        i++;
+        if( (typeof(topic)) === 'string' && (typeof(topic)) != null && (typeof(cash)) != null 
+        && topic != '' && cash!='' && topic.length <=50 )        
+        {
+            console.log('done');
+            appData.expenses[topic] = cash;
+        } else {
+            i=i-1;
+        }
+        
+} while(i < 2);
+appData.moneyPerDay = appData.budget/30
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-// console.log(typeof("d" + 54));
+if(appData.moneyPerDay < 400) console.log('Минимальный уровень домтатка');
+else if(appData.moneyPerDay >= 400 && appData.moneyPerDa < 1000) console.log('Средний уровень достатка');
+else if(appData.moneyPerDay >= 1000) console.log('Высокий уровень достатка');
+else console.log('Ошибка');
 
-// console.log(5+ +true);
-
-// console.log(parseInt('15px', 10));
-// let answer = prompt('Есть ли вам 18 лет', '');
-let incr = 10,
-    decr = 10;
-incr++;
---decr;
-console.log(incr);
-console.log(decr);
